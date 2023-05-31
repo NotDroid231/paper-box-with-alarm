de2e7dc5a4aaa9debf2772300176df9a8e2aad81
droidbetter <98360187+NotDroid231@users.noreply.github.com>
2023-05-31 11:35:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    button_a += 1
+    if (button_a % 2 == 1) {
+        let inifinty = 0
+        for (let index = 0; index < inifinty; index++) {
+        	
+        }
+    } else if (button_a % 2 == 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    button_ab += 1
+    if (button_ab % 2 == 1) {
+    	
+    } else if (button_ab % 2 == 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    button_b += 1
+    if (button_b % 2 == 1) {
+    	
+    } else if (button_b % 2 == 0) {
+    	
+    }
+})
+let button_b = 0
+let button_ab = 0
+let button_a = 0
+radio.setGroup(69420)
+button_a = 0
+loops.everyInterval(60000, function () {
+    button_a += -10
 })
